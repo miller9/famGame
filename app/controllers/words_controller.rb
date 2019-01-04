29ff4ee029b05f91b01cde77c4982548c47830eb
miller <millerpoli9@gmail.com>
2019-01-04 13:51:24 -0500

    Creating index page to show words/categories saved

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,9 +1,27 @@
 class WordsController < ApplicationController
 
 	def index
+		@words = Word.all.order('created_at DESC')
 	end
 
 	def new
+		@word = Word.new
 	end
-	
+
+	def create
+		@word = Word.new(word_params)
+		
+		if @word.save
+			redirect_to @word
+		else
+			render 'new'
+		end
+	end
+
+
+	private
+
+		def word_params
+			params.require(:word).permit(:title, :category)
+		end
 end
